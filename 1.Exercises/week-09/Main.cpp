9fddae5c462ba6d97030a1710001e3ad5c87eaa2
Gyokan <gyokan.nezhdetov@gmail.com>
2018-05-03 22:27:02 +0300

    Added getLargestHouse function

--- 1.Exercises/week-09/Main.cpp
+++ 1.Exercises/week-09/Main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 #include "House.h"
 
+House* getLargestHouse(House* houses, int length)
+{
+	double largest = 0;
+	double current = 0;
+	House* largestHouse = nullptr;
+	for (int i = 0; i < length; i++)
+	{
+		current = houses[i].getHeight() / houses[i].getStoreys();
+		if (current > largest)
+		{
+			largestHouse = &houses[i];
+			largest = current;
+		}
+	}
+
+	return largestHouse;
+}
+
 int main()
 {
 	Building building(30, 650.43, "ul. \"8-mi dekemvri\"");
@@ -10,6 +28,18 @@ int main()
 	House house(5, 125.34, "ul. Hirsto Botev 123", 2, "Ivan Ivanov");
 	house.print();
 	std::cout << "----------\n";
+
+	House houseTwo(10, 154, "ul. Hirsto Botev 124", 3, "Ivan Petrov");
+	House houseThree(10, 165, "ul. Hirsto Botev 124", 4, "Ivan Petrov");
+
+	House houses[3];
+	houses[0] = house;
+	houses[1] = houseTwo;
+	houses[2] = houseThree;
+
+	std::cout << "Largest house:\n";
+	House* largest = getLargestHouse(houses, 3);
+	largest->print();
 
 	// Building inputBuilding;
 	// inputBuilding.read();
